ad0875f4e469ceb3c05b45a4f185f9c8dea3df36
KosAqH <marcin_wlodarczyk@protonmail.com>
2026-04-18 12:10:11 +0200

    Agent with memory

---
+++ langchain-foundation-course/module_1/05_agent_memory.py
@@ -0,0 +1,82 @@
+import dotenv
+dotenv.load_dotenv()
+
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openrouter import ChatOpenRouter
+
+from langgraph.graph import START, StateGraph, MessagesState
+from langgraph.prebuilt import tools_condition, ToolNode
+
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+def divide(a: int, b: int) -> float:
+    """Divide a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
+
+tools = [add, multiply, divide]
+
+# Define LLM with bound tools
+llm = ChatOpenRouter(model="openai/gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
+
+# System message
+sys_msg = SystemMessage(content="You are a helpful assistant tasked with writing performing arithmetic on a set of inputs.")
+
+# Node
+def assistant(state: MessagesState):
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+
+# Add memory and compile
+from langgraph.checkpoint.memory import MemorySaver
+memory = MemorySaver()
+react_graph_memory = builder.compile(checkpointer=memory)
+
+# Specify a thread
+config = {"configurable": {"thread_id": "1"}}
+
+# Specify an input
+messages = [HumanMessage(content="Add 3 and 4.")]
+
+# Run
+messages = react_graph_memory.invoke({"messages": messages}, config)
+for m in messages['messages']:
+    m.pretty_print()
+
+user_prompt = input("Enter a math problem for the assistant to solve using its tools: ")
+messages = react_graph_memory.invoke({"messages": [HumanMessage(content=user_prompt)]}, config)
+for m in messages['messages']:
+    m.pretty_print()
